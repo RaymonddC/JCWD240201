bdae5c226eec05534049a385895c5fbbf576035d
EnzoOestanto <enzo.oestanto@gmail.com>
2023-07-11 10:01:03 +0700

    Merge branch 'SetupProject' of https://github.com/masdefry/JCWD240201 into SetupProject

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,29 +1,29 @@
-import axios from "axios";
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import SignIn from "./Pages/test";
+import SignIn from './Pages/test';
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    (async () => {
-      const { data } = await axios.get(
-        `${process.env.REACT_APP_API_BASE_URL}/greetings`
-      );
-      setMessage(data?.message || "");
-    })();
+    // (async () => {
+    //   const { data } = await axios.get(
+    //     `${process.env.REACT_APP_API_BASE_URL}/greetings`
+    //   );
+    //   setMessage(data?.message || "");
+    // })();
   }, []);
   return (
     <>
       <Routes>
-        <Route path='/' element={<SignIn />} />
+        <Route path="/" element={<SignIn />} />
       </Routes>
     </>
   );
--- projects/client/src/Pages/test.jsx
+++ projects/client/src/Pages/test.jsx
@@ -12,6 +12,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
 
 function Copyright(props) {
   return (
@@ -31,6 +32,8 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function SignIn() {
+  const user = useSelector((state) => state.user.user);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -59,36 +62,10 @@ export default function SignIn() {
             Sign in
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
+            <TextField margin="normal" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" autoFocus />
+            <TextField margin="normal" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+            <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
               Sign In
             </Button>
             <Grid container>
