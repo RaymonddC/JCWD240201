df722e1ba20240344ca031f4761f7a9875a3a4fa
RaymonddC <raymondchrisandy@gmail.com>
2023-07-11 16:06:43 +0700

    SetupProjectDone

---
+++ projects/client/src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#000000',
+    },
+  },
+});
+
+export default defaultTheme;
