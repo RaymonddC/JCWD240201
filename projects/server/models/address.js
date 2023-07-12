6ad5011b875f182597c5da67b3d11a7903ffb9a7
Eggi19 <eggiyapari19@gmail.com>
2023-07-12 15:45:56 +0800

    SetupProject_Done

--- projects/server/models/address.js
+++ projects/server/models/address.js
@@ -14,11 +14,13 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   address.init({
+    user_id: DataTypes.INTEGER,
     city_id: DataTypes.INTEGER,
     notes: DataTypes.STRING,
     address: DataTypes.STRING,
     phone_number: DataTypes.STRING,
     reciever: DataTypes.STRING,
+    is_main: DataTypes.BOOLEAN,
     deletedAt: DataTypes.DATE
   }, {
     sequelize,
