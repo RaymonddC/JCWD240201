a0c8c8b16e8ecbcc28a6ef849aa7df2b756d9a46
Eggi19 <eggiyapari19@gmail.com>
2023-07-13 16:01:37 +0800

    Eggi_VerifyAccount_Update

--- projects/client/src/Pages/VerifyEmail.jsx
+++ projects/client/src/Pages/VerifyEmail.jsx
@@ -7,29 +7,27 @@ import Container from '@mui/material/Container';
 import { ThemeProvider } from '@mui/material/styles';
 import defaultTheme from '../utils/theme';
 import { Button } from '@mui/material';
-import { useEffect } from "react"
-import { useSearchParams } from "react-router-dom"
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { userVerification } from '../API/auth';
 
 // TODO remove, this demo shouldn't need to reset the theme.
 
-const theme = defaultTheme
+const theme = defaultTheme;
 
 export default function VerifyEmail() {
-  const [searchParams] = useSearchParams()
+  const [searchParams] = useSearchParams();
 
-    const verifiedAccount = async () => {
-        try {
-            let token = searchParams.get('token')
-            await userVerification(token)
-        } catch (error) {
+  const verifiedAccount = async () => {
+    try {
+      let token = searchParams.get('token');
+      await userVerification(token);
+    } catch (error) {}
+  };
 
-        }
-    }
-
-    useEffect(() => {
-        verifiedAccount()
-    }, [])
+  useEffect(() => {
+    verifiedAccount();
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
@@ -42,12 +40,18 @@ export default function VerifyEmail() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ width: 100, height: 100, m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar
+            sx={{ width: 100, height: 100, m: 1, bgcolor: 'secondary.main' }}
+          >
             <CheckIcon style={{ fontSize: '5rem' }} />
           </Avatar>
-          <div className='text-3xl font-semibold my-5'>Verified!</div>
-          <div className='text-slate-400 font-medium mb-5'>You have successfully verified account</div>
-          <Button variant='contained' color='secondary' sx={{ color: 'white' }}>Ok</Button>
+          <div className="text-3xl font-semibold my-5">Verified!</div>
+          <div className="text-slate-400 font-medium mb-5">
+            You have successfully verified account
+          </div>
+          <Button variant="contained" color="secondary" sx={{ color: 'white' }}>
+            Ok
+          </Button>
         </Box>
       </Container>
     </ThemeProvider>
