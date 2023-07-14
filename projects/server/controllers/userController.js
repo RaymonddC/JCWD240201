42bbeee033c4431e0d32dec6896688657f574ac7
Andika Dwi Abizar <andikabizar2004@gmail.com>
2023-07-14 09:56:02 +0700

    Save before pull from main

--- projects/server/controllers/userController.js
+++ projects/server/controllers/userController.js
@@ -1,16 +1,61 @@
-const { user } = require('./../models');
+const { user, role } = require('./../models');
 
 // const User = db.user;
 
 const sequelize = require('sequelize');
 
+const validate = (data) => {
+  if (
+    !data.full_name ||
+    !data.phone_number ||
+    !data.gender ||
+    !data.birthdate
+  ) {
+    throw { message: 'Please fill your form correctly', code: 400 };
+  }
+};
+
 module.exports = {
   getUserData: async (req, res) => {
     try {
-      const hehe = await user.findAll();
+      //TODO: get user_id from token
+      const hehe = await user.findOne({
+        include: { model: role, attributes: ['role_name'] },
+        attributes: {
+          exclude: ['password', 'username'],
+        },
+        where: {
+          id: 1,
+        },
+      });
       res.send(hehe);
     } catch (error) {
-      res.send(error);
+      res.send(error.message || error);
+    }
+  },
+  updateUserData: async (req, res) => {
+    try {
+      const { full_name, phone_number, gender, birthdate } = req.body;
+      //TODO: get user_id from token
+      validate({ full_name, phone_number, gender, birthdate });
+
+      const updateData = await user.update(
+        {
+          full_name,
+          phone_number,
+          gender,
+          birthdate,
+        },
+        {
+          where: {
+            id: 1,
+          },
+        },
+      );
+
+      res.status(200).send(updateData);
+    } catch (error) {
+      res.status(error.code).send(error.message || error);
     }
   },
 };
--- projects/server/routers/userRoute.js
+++ projects/server/routers/userRoute.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const { userController } = require('../controllers');
+const { APIKey } = require('../middleware/APIKey');
 
-router.get('/', userController.getUserData);
+router.get('/', APIKey, userController.getUserData);
+router.put('/', APIKey, userController.updateUserData);
 
 module.exports = router;
