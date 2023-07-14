08fc2024f155120ce935d7f4e5536f31ca0c8c5b
RaymonddC <raymondchrisandy@gmail.com>
2023-07-15 02:20:21 +0700

    Update authController.js

--- projects/server/controllers/authController.js
+++ projects/server/controllers/authController.js
@@ -14,85 +14,79 @@ const {
   generateToken,
 } = require('../helpers/userHelper');
 
-module.exports = {
-  sendVerifyEmail: async (req, res) => {
-    try {
-      const { email } = req.body;
-      const isEmail = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
-      let payload = { email: email };
-      const token = jwt.sign(payload, 'verification-account');
-      const data = fs.readFileSync(
-        './helpers/verifyEmailTemplate.html',
-        'utf-8',
-      );
-      const tempCompile = await Handlebars.compile(data);
-      const tempResult = tempCompile({ token: token });
+const sendVerifyEmail = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const isEmail = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+    let payload = { email: email };
+    const token = jwt.sign(payload, 'verification-account');
+    const data = fs.readFileSync('./helpers/verifyEmailTemplate.html', 'utf-8');
+    const tempCompile = await Handlebars.compile(data);
+    const tempResult = tempCompile({ token: token });
 
-      if (isEmail.test(email)) {
-        await transporter.sendMail({
-          from: 'pharmacy.jcwd2402@gmail.com',
-          to: email,
-          subject: 'Account Verification',
-          html: tempResult,
-        });
-
-        return res.send({
-          success: true,
-          status: 200,
-          message: 'Send Verification Email Success',
-          data: null,
-        });
-      } else {
-        throw { status: 400, message: 'email is not valid' };
-      }
-    } catch (error) {
-      return res.send({
-        success: false,
-        status: error.status,
-        message: error.message,
-        data: null,
+    if (isEmail.test(email)) {
+      await transporter.sendMail({
+        from: 'pharmacy.jcwd2402@gmail.com',
+        to: email,
+        subject: 'Account Verification',
+        html: tempResult,
       });
-    }
-  },
-
-  verifyAccount: async (req, res) => {
-    try {
-      let token = req.headers.authorization;
-      token = token.split(' ')[1];
-      let verifiedUser = jwt.verify(token, 'verification-account');
-
-      if (!verifiedUser) throw { message: 'Unauthorized request', status: 401 };
-
-      const isVerified = await User.findOne({
-        where: { email: verifiedUser.email },
-      });
-
-      if (isVerified.verified) throw { message: 'Account is already verified' };
-
-      const result = await User.update(
-        { verified: true },
-        {
-          where: {
-            email: verifiedUser.email,
-          },
-        },
-      );
 
       return res.send({
         success: true,
         status: 200,
-        message: 'Verification Email Success',
-        data: result,
-      });
-    } catch (error) {
-      return res.send({
-        success: false,
-        status: error.status,
-        message: error.message,
+        message: 'Send Verification Email Success',
         data: null,
       });
+    } else {
+      throw { status: 400, message: 'email is not valid' };
     }
-  },
+  } catch (error) {
+    return res.send({
+      success: false,
+      status: error.status,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+const verifyAccount = async (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    token = token.split(' ')[1];
+    let verifiedUser = jwt.verify(token, 'verification-account');
+
+    if (!verifiedUser) throw { message: 'Unauthorized request', status: 401 };
+
+    const isVerified = await User.findOne({
+      where: { email: verifiedUser.email },
+    });
+
+    if (isVerified.verified) throw { message: 'Account is already verified' };
+
+    const result = await User.update(
+      { verified: true },
+      {
+        where: {
+          email: verifiedUser.email,
+        },
+      },
+    );
+
+    return res.send({
+      success: true,
+      status: 200,
+      message: 'Verification Email Success',
+      data: result,
+    });
+  } catch (error) {
+    return res.send({
+      success: false,
+      status: error.status,
+      message: error.message,
+      data: null,
+    });
+  }
 };
 const userCreate = async (req, res, next) => {
   try {
@@ -199,4 +193,4 @@ const userLogin = async (req, res, next) => {
   }
 };
 
-module.exports = { userCreate, userLogin };
+module.exports = { sendVerifyEmail, verifyAccount, userCreate, userLogin };
