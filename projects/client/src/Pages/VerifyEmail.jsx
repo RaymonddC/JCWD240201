1d63c6b3fb57bcb82671751ae5ce5c2bcff17542
Eggi19 <eggiyapari19@gmail.com>
2023-07-14 11:23:39 +0800

    Eggi_VerifyAccount_Update

--- projects/client/src/Pages/VerifyEmail.jsx
+++ projects/client/src/Pages/VerifyEmail.jsx
@@ -1,11 +1,6 @@
-import defaultTheme from '../utils/theme';
 import { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { userVerification } from '../API/auth';
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const theme = defaultTheme;
 
 export default function VerifyEmail() {
   const [searchParams] = useSearchParams();
