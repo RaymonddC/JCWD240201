022a9e81df9b89082bb14bdaa9f8a1dfb6e83369
Eggi19 <eggiyapari19@gmail.com>
2023-07-14 16:23:43 +0800

    Eggi_VerifyAccount_Update

--- projects/client/src/API/auth.js
+++ projects/client/src/API/auth.js
@@ -1,14 +1,33 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const URL = process.env.REACT_APP_API_BASE_URL
-const APIKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6ImFjY2Vzcy1jb25maXJtIiwiaWF0IjoxNjg5MTQ4NTc3fQ.sjK_BgX2XeIcj2qdk16kGOY8kLp1QnaPrQ9z1r_Q5B4'
+const URL = process.env.REACT_APP_API_BASE_URL;
+const APIKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6ImFjY2Vzcy1jb25maXJtIiwiaWF0IjoxNjg5MTQ4NTc3fQ.sjK_BgX2XeIcj2qdk16kGOY8kLp1QnaPrQ9z1r_Q5B4';
 
 export function userVerification(token) {
-    console.log(URL);
-    return axios.post(`${URL}auth/verifyEmail`, {}, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            apiKey: APIKey
-        }
-    })
+  console.log(URL);
+  return axios.post(
+    `${URL}auth/verifyEmail`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        apiKey: APIKey,
+      },
+    },
+  );
+}
+
+export function sendVerificationEmail(email) {
+  console.log(URL);
+  return axios.post(
+    `${URL}auth/sendVerify`,
+    {
+      email: email,
+    },
+    {
+      headers: {
+        apiKey: APIKey,
+      },
+    },
+  );
 }
