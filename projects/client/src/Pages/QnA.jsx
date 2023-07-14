dd6a858ad02409e5654d79bed066bc90230c895e
EnzoOestanto <enzo.oestanto@gmail.com>
2023-07-14 22:04:35 +0700

    QnAUser

--- projects/client/src/Pages/QnA.jsx
+++ projects/client/src/Pages/QnA.jsx
@@ -4,6 +4,16 @@ export default function QnAPage() {
   return (
     <>
       <NavBar />
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Ask a question</span>
+          <span className="label-text-alt">Alt label</span>
+        </label>
+        <textarea
+          className="textarea textarea-bordered h-24"
+          placeholder="Bio"
+        ></textarea>
+      </div>
     </>
   );
 }
