50395ce7b48a9c6838278164265edd4fe44dc58c
Eggi19 <eggiyapari19@gmail.com>
2023-07-16 23:43:29 +0800

    Eggi_ResetPassword_Update

--- projects/server/routers/authRoute.js
+++ projects/server/routers/authRoute.js
@@ -8,6 +8,6 @@ const APIKey = require('../middleware/APIKey');
 router.post('/sendVerify', APIKey.APIKey, authController.sendVerifyEmail)
 router.post('/verifyEmail', APIKey.APIKey, authController.verifyAccount)
 router.post('/sendReset', APIKey.APIKey, authController.sendResetPasswordForm)
-router.post('/resetPassword', APIKey.APIKey, authController.resetPassword)
+router.patch('/resetPassword', APIKey.APIKey, authController.resetPassword)
 
 module.exports = router;
