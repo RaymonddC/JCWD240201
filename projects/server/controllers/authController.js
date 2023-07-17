a6eb250a7a86120c0866f34a87caa4f30d8f687c
RaymonddC <raymondchrisandy@gmail.com>
2023-07-17 10:09:20 +0700

    Update authController.js

--- projects/server/controllers/authController.js
+++ projects/server/controllers/authController.js
@@ -89,6 +89,7 @@ const verifyAccount = async (req, res, next) => {
     });
   }
 };
+
 const userCreate = async (req, res, next) => {
   try {
     const {
@@ -113,7 +114,7 @@ const userCreate = async (req, res, next) => {
       throw { message: 'Fill all data', code: 400 };
 
     const isEmailExist = await getUser(email, username);
-
+    // process.exit();
     if (isEmailExist)
       throw { message: 'username or email is already exists', code: 400 };
 
@@ -132,7 +133,7 @@ const userCreate = async (req, res, next) => {
       role_id: role ? role : 2,
     });
 
-    const result = await getUser(newUser.email, newUser.username);
+    const result = await getUser(newUser.email, newUser.username, 'password');
 
     req.params.username = result.username;
 
@@ -222,67 +223,3 @@ module.exports = {
   userLogin,
   getUserById,
 };
-const userCreate = async (req, res, next) => {
-  try {
-    const {
-      fullName,
-      username,
-      email,
-      password,
-      confirmPassword,
-      phoneNumber,
-      role,
-    } = req.body;
-    console.log(req.body.username);
-
-    if (
-      !fullName ||
-      !username ||
-      !email ||
-      !password ||
-      !confirmPassword ||
-      !phoneNumber
-    )
-      throw { message: 'Fill all data', code: 400 };
-
-    const isEmailExist = await getUser(email, username);
-    // process.exit();
-    if (isEmailExist)
-      throw { message: 'username or email is already exists', code: 400 };
-
-    const passwordError = await validatePassword(password, confirmPassword);
-    if (passwordError) throw passwordError;
-
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
-
-    let newUser = await User.create({
-      full_name: fullName,
-      username,
-      email,
-      password: hashPassword,
-      phone_number: phoneNumber,
-      role_id: role ? role : 2,
-    });
-
-    const result = await getUser(newUser.email, newUser.username, 'password');
-
-    req.params.username = result.username;
-
-    // next();
-    return res.status(201).send({
-      success: true,
-      message: 'Register Success',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-    // res.status(error.code || 500).send({
-    //   success: false,
-    //   message: error.message,
-    //   data: null,
-    // });
-  }
-};
-
-module.exports = { userCreate };
