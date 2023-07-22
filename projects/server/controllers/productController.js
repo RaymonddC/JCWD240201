49054161e61c7660c756e52c3eeeb4c3794d987d
Eggi19 <eggiyapari19@gmail.com>
2023-07-22 23:41:06 +0800

    Eggi_UpdateProduct_Update

--- projects/server/controllers/productController.js
+++ projects/server/controllers/productController.js
@@ -13,25 +13,37 @@ const Label = db.label;
 const { sequelize } = require('../models');
 const deleteFiles = require('../helpers/deleteFiles');
 
-const getProducts = async (req, res, next) => {
+const getAllProducts = async (req, res, next) => {
   try {
     const { page, search, category, limit } = req.query;
-    req.query;
+    // console.log(req.query);
     const pageLimit = Number(limit);
-    pageLimit, '<<';
-    const offset = (Number(page) - 1) * pageLimit;
-    offset;
-    let response = await questionDB.findAndCountAll({
+    const offset = (Number(page) - 1) * pageLimit + 1;
+    let where = undefined;
+    let order;
+    if (search !== 'undefined') {
+      where = {};
+      where.name = { [Op.like]: `%${search}%` };
+    }
+    if (category !== 'undefined') {
+      where = {};
+      where.category_id = category;
+    }
+    const response = await productDB.findAndCountAll({
+      include: labelDB,
       limit: pageLimit,
       offset: offset,
-      order: [['updatedAt', 'DESC']],
+      where: where,
+      order: [['name', 'ASC']],
     });
-    response;
-    const totalPage = Math.ceil(response.count / pageLimit);
-    totalPage;
+    const totalPage = Math.ceil((response.count - 1) / pageLimit);
+    // console.log(pageLimit, '<<');
+    // console.log(response);
+    // console.log(offset);
+    // console.log(totalPage);
     return res.status(200).send({
       success: true,
-      message: 'get Products success',
+      message: 'get all products success',
       totalPage: totalPage,
       data: response,
     });
@@ -43,104 +55,19 @@ const getProducts = async (req, res, next) => {
     // });
   }
 };
-
-const createQuestion = async (req, res) => {
-  const { question, user_id } = req.body;
-  ('question');
-  try {
-    let result = await questionDB.create({ question, user_id });
-
-    return res.status(201).send({
-      success: true,
-      message: 'Your question was created successfully',
-      data: result,
-    });
-  } catch (error) {
-    return res.status(400).send({
-      success: false,
-      message: error.message,
-    });
-  }
-};
-
-const getProduct = async (req, res, next) => {
+const getProductDetails = async (req, res, next) => {
   try {
     const { id } = req.params;
-
-    // let result = await Product.findOne({
-    //   include: [
-    //     {
-    //       model: LikeTweet,
-    //       attributes: ['user_id'],
-    //     },
-    //     {
-    //       model: User,
-    //       attributes: ['username', 'official', 'profilePicture', 'fullname'],
-    //     },
-    //   ],
-    //   where: {
-    //     id: id,
-    //   },
-    // });
-
-    // if (!result) throw { message: 'Tweet not found', code: 400 };
+    const response = await productDB.findOne({
+      where: { id },
+    });
 
     return res.status(200).send({
       success: true,
-      message: 'Tweet Found',
-      data: result,
+      message: 'get product details success',
+      data: response,
     });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const getAllProduct = async (req, res, next) => {
-  try {
-    ('backend');
-    let {
-      searchCategory,
-      ordered,
-      orderedBy,
-      search,
-      page = 1,
-      limitPage = 3,
-    } = req.query;
-
-    let whereQuery = {
-      //   caption: { [Op.like]: `%${search || ''}%` },
-      //   reply_id: { [Op.eq]: null },
-    };
-
-    // if (searchCategory) whereQuery['category_id'] = searchCategory;
-
-    const { count, rows } = await Product.findAndCountAll({
-      //   include: [
-      //     {
-      //       model: LikeTweet,
-      //       attributes: ['user_id'],
-      //     },
-      //     {
-      //       model: User,
-      //       attributes: ['username', 'official', 'profilePicture', 'fullname'],
-      //     },
-      //   ],
-      where: whereQuery,
-      order: [['createdAt', 'DESC']],
-      limit: Number(limitPage),
-      offset: (Number(page) - 1) * limitPage,
-    });
-    count;
-
-    return res.status(200).send({
-      success: true,
-      message: 'getAll Tweet',
-      data: rows,
-      pageCount: count,
-    });
-  } catch (error) {
-    next(error);
-  }
+  } catch (error) {}
 };
 
 const createProduct = async (req, res) => {
@@ -324,10 +251,8 @@ const updateProductImage = async (req, res) => {
 };
 
 module.exports = {
-  createQuestion,
-  getProducts,
-  getProduct,
-  getAllProduct,
+  getAllProducts,
+  getProductDetails,
   createProduct,
   deleteProduct,
   updateProduct,
