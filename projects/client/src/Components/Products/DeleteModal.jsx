bc990fefa66574a58e41a86495f6b48e93cf0899
Eggi19 <eggiyapari19@gmail.com>
2023-07-25 17:40:07 +0800

    Eggi_UpdateProduct_Update

--- projects/client/src/Components/Products/DeleteModal.jsx
+++ projects/client/src/Components/Products/DeleteModal.jsx
@@ -4,21 +4,21 @@ export default function DeleteModal() {
   return (
     <>
       {/* The button to open modal */}
-      <label htmlFor="my_modal_6" className="btn">
-        <MdDeleteOutline/>
+      <label htmlFor="my_modal_6" className="btn btn-accent">
+        <MdDeleteOutline size={30}/>
       </label>
 
       {/* Put this part before </body> tag */}
       <input type="checkbox" id="my_modal_6" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">This modal works with a hidden checkbox!</p>
+          <h3 className="font-bold text-lg">Delete Product</h3>
+          <p className="py-4">Are you sure want to delete product?</p>
           <div className="modal-action">
             <label htmlFor="my_modal_6" className="btn">
-              Close!
+              Close
             </label>
-            <label htmlFor="my_modal_6" className="btn">
+            <label htmlFor="my_modal_6" className="btn btn-accent">
               Delete
             </label>
           </div>
