47952b30089cc5c237dfe5b4980a6b6eb4001385
Eggi19 <eggiyapari19@gmail.com>
2023-07-26 18:03:31 +0800

    Eggi_UpdateProduct_Update

--- projects/client/src/Pages/EditProduct.jsx
+++ projects/client/src/Pages/EditProduct.jsx
@@ -1,6 +1,9 @@
 import { useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { getProductDetailsAPI } from '../API/productAPI';
+import { getProductDetailsAPI, updateProduct } from '../API/productAPI';
+import { useFormik } from 'formik';
+import { validateAddProduct } from '../Helper/productHelper';
+import { toast } from 'react-hot-toast';
 
 export default function EditProduct() {
   const [searchParams] = useSearchParams();
@@ -15,6 +18,42 @@ export default function EditProduct() {
       setImage(result?.data?.image?.image)
     } catch (error) {}
   };
+
+  const formik = useFormik({
+    initialValues: {
+      product: {
+        name: '',
+        packaging_type_id: 0,
+        product_type_id: 0,
+        net_content: '',
+        description: '',
+        dosing: '',
+        BPOM_id: '',
+        require_prescription: null,
+        price: 0,
+      },
+      category: {
+        category_id: [],
+      },
+      image: {
+        product: '',
+      },
+    },
+    validate: validateAddProduct,
+    onSubmit: async (values, { setSubmitting }) => {
+      try {
+        const result = await updateProduct(values);
+        const errorMessage = { message: result.data.message };
+        if (result.data.success) {
+          toast.success(result.data.message);
+          setSubmitting(false);
+        }
+        throw errorMessage;
+      } catch (error) {
+        toast.success(error.message);
+      }
+    },
+  });
 
   useEffect(() => {
     getCurrentData()
