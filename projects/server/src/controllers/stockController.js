847e6b4378221f2fa5c50ff3e25739a847a69251
EnzoOestanto <enzo.oestanto@gmail.com>
2023-08-01 20:00:18 +0700

    UnitConversion

--- projects/server/src/controllers/stockController.js
+++ projects/server/src/controllers/stockController.js
@@ -106,11 +106,16 @@ const unitConversion = async (req, res, next) => {
   try {
     console.log('>>> unit conversion');
     const { product_id, qty } = req.body;
+    let newOpenedStock;
     let openedStock;
     let resOpenedStock1;
-    let stock;
+    let newClosedStock;
     if (!product_id) throw { message: 'please provide a product' };
     if (!qty) throw { message: 'please provide quantity' };
+    const resStockHistoryType = await stockHistoryTypeDB.findOne({
+      where: { type: 'unit conversion' },
+    });
+    const stock_history_type_id = resStockHistoryType.id;
     const resOpenedStock = await openedStockDB.findOne({
       where: { product_id: product_id },
     });
@@ -128,47 +133,81 @@ const unitConversion = async (req, res, next) => {
       openedStock = 0;
     }
 
+    const packaging_type = response.packaging_type.type_name;
+    const unit = response.product_type.unit;
     const closedStock = resClosedStock.total_stock;
     const netContent = response.net_content;
+    newClosedStock = closedStock;
+    newOpenedStock = openedStock;
+    let openedFromStock = 0;
 
-    while (openedStock < qty) {
+    if (openedStock < qty) {
+      while (newOpenedStock < qty) {
+        newClosedStock--;
+        openedFromStock++;
+        newOpenedStock = newOpenedStock + openedFromStock * netContent;
+      }
+
       if (openedStock === null) {
         resOpenedStock1 = await openedStockDB.create(
           {
             product_id,
-            qty: netContent,
+            qty: newOpenedStock,
           },
           { transaction: t },
         );
         openedStock = resOpenedStock1.qty;
       } else {
-        const newOpenedStock = netContent + openedStock;
         resOpenedStock1 = await openedStockDB.update(
           { qty: newOpenedStock },
           { where: { product_id } },
           { transaction: t },
         );
-        const openedStockCheck = await openedStockDB.findOne({
-          where: { product_id },
-        });
-        openedStock = openedStockCheck.qty;
       }
-      const newClosedStock = closedStock - 1;
-      updateStock = await closedStockDB.update(
+      const updateClosedStock = await closedStockDB.update(
         { total_stock: newClosedStock },
         { where: { product_id } },
         { transaction: t },
       );
-      // const resUpdated = await closedStockDB.findOne({ where: { product_id } });
-      // stock = resUpdated.total_stock;
+      const updateStockHystoryClosedOut = await stockHistoryDB.create(
+        {
+          product_id,
+          unit: 0,
+          qty: openedFromStock,
+          action: 'out',
+          stock_history_type_id,
+          total_stock: newClosedStock,
+        },
+        { transaction: t },
+      );
+      const addOpenStock = openedFromStock * netContent;
+      const updateStockHystoryOpenIn = await stockHistoryDB.create({
+        product_id,
+        unit: 1,
+        qty: addOpenStock,
+        action: 'in',
+        stock_history_type_id,
+        total_stock: newOpenedStock,
+      });
     }
-    const newUpdateStock = openedStock - qty;
-    const updateStock = await openedStockDB.update(
+
+    const newUpdateStock = newOpenedStock - qty;
+    const updateOpenStock = await openedStockDB.update(
       { qty: newUpdateStock },
       { where: { product_id } },
       { transaction: t },
     );
-
+    const updateStockHystoryOpenOut = await stockHistoryDB.create(
+      {
+        product_id,
+        unit: 1,
+        qty,
+        action: 'out',
+        stock_history_type_id,
+        total_stock: newUpdateStock,
+      },
+      { transaction: t },
+    );
     // console.log('unit conversion', response);
     await t.commit();
     return res.status(200).send({
@@ -178,7 +217,7 @@ const unitConversion = async (req, res, next) => {
       net_content: netContent,
       closed_stock: closedStock,
       opened_stock: resOpenedStock1,
-      updateStock: stock,
+      type: stock_history_type_id,
     });
   } catch (error) {
     // await t.rollback();
@@ -186,8 +225,4 @@ const unitConversion = async (req, res, next) => {
   }
 };
 
-module.exports = {
-  createDataStock,
-  getStockHistoryType,
-  unitConversion,
-};
+module.exports = { createDataStock, getStockHistoryType, unitConversion };
