900b530350a357dd4cdb771f6ca1c5233c6da619
EnzoOestanto <enzo.oestanto@gmail.com>
2023-08-22 14:45:30 +0700

    add custom theme

--- projects/client/tailwind.config.js
+++ projects/client/tailwind.config.js
@@ -14,6 +14,21 @@ module.exports = {
     require('daisyui'),
   ],
   daisyui: {
-    themes: ['emerald'],
+    themes: [
+      'emerald',
+      {
+        mytheme: {
+          primary: '#ff5757',
+          secondary: '#769afc',
+          accent: '#96dd11',
+          neutral: '#15181e',
+          'base-100': '#ffffff',
+          info: '#72acf3',
+          success: '#27e7a1',
+          warning: '#b6910c',
+          error: '#e73b32',
+        },
+      },
+    ],
   },
 };
