20c8182fc9d0d9dd7dd94fac5c596502889c81ae
RaymonddC <raymondchrisandy@gmail.com>
2023-08-31 22:15:13 +0700

    seed

---
+++ projects/server/src/migrations/8-create-packaging-type.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('packaging_types', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      type_name: {
+        type: Sequelize.STRING,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('packaging_types');
+  },
+};
