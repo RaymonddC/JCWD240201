132b3b00c5b5011193aee09a12ba4fa884b8a727
EnzoOestanto <enzo.oestanto@gmail.com>
2023-09-03 18:24:25 +0700

    BugFixing

--- projects/server/src/controllers/cartController.js
+++ projects/server/src/controllers/cartController.js
@@ -31,9 +31,7 @@ const getCarts = async (req, res, next) => {
     } = req.query;
 
     let whereQuery = { user_id: userId };
-
     const { count, rows } = await getUserCarts('', whereQuery);
-
     const newRows = await Promise.all(
       rows.map(async (row) => {
         if (row.product_id === 1 && row.confirmation === true) {
@@ -137,11 +135,6 @@ const updateCart = async (req, res, next) => {
     const { id } = req.params;
     const { qty, confirmation = false, isCheck = true } = req.body;
     const userId = req.user.id;
-
-    // const product = await getProduct()
-    // if(product.stock < qty) throw({message:'kebanyakan bro belinya'})
-    // if(product. === true) throw({message:'butuh resep bro'})
-
     const isCart = await getCartByPk(id, '');
     if (!isCart) throw { code: 400, message: 'Cart Not Found' };
 
