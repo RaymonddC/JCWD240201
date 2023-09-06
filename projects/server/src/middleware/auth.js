2ffb82a1fb08eba0f137ddb03cb6aa2166f6ac70
EnzoOestanto <33096825+EnzoOestanto@users.noreply.github.com>
2023-09-06 15:44:14 +0700

    Update auth.js

--- projects/server/src/middleware/auth.js
+++ projects/server/src/middleware/auth.js
@@ -9,7 +9,7 @@ module.exports = {
     if (!token) {
       return res.status(401).send({
         success: false,
-        message: 'Unauthorized',
+        message: 'Unauthorized1',
         data: null,
       });
     }
@@ -17,10 +17,10 @@ module.exports = {
     try {
       token = token.split(' ')[1];
       if (token === null || !token)
-        throw { message: 'Unauthorized', code: 401 };
+        throw { message: 'Unauthorized2', code: 401 };
       let verifyUser = jwt.verify(token, 'pharmacy');
 
-      if (!verifyUser) throw { message: 'Unauthorized', code: 401 };
+      if (!verifyUser) throw { message: 'Unauthorized3', code: 401 };
 
       req.user = verifyUser;
       next();
